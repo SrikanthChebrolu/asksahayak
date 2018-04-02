44ab16d69530a0ff504b222c895ed03356d207bb
Srikanth Chebrolu <srikanthchebrolu3333@gmail.com>
2018-04-02 01:14:19 -0700

    made small changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,131 +1,91 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import IconButton from 'material-ui/IconButton';
-import MenuIcon from 'material-ui-icons/Menu';
 
-const styles = {
-    root: {
-        flexGrow: 1,
-    },
-    flex: {
-        flex: 1,
-    },
-    menuButton: {
-        marginLeft: -12,
-        marginRight: 20,
-    },
+const LoggedOutView = props => {
+  if (!props.currentUser) {
+    return (
+      <ul className="nav navbar-nav pull-xs-right">
+
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            Sign in
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/register" className="nav-link">
+            Sign up
+          </Link>
+        </li>
+
+      </ul>
+    );
+  }
+  return null;
 };
 
+const LoggedInView = props => {
+  if (props.currentUser) {
+    return (
+      <ul className="nav navbar-nav pull-xs-right">
+
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/editor" className="nav-link">
+            <i className="ion-compose"></i>&nbsp;New Post
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/settings" className="nav-link">
+            <i className="ion-gear-a"></i>&nbsp;Settings
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link
+            to={`/@${props.currentUser.username}`}
+            className="nav-link">
+            <img src={props.currentUser.image} className="user-pic" alt={props.currentUser.username} />
+            {props.currentUser.username}
+          </Link>
+        </li>
+
+      </ul>
+    );
+  }
+
+  return null;
+};
 
 class Header extends React.Component {
-    state = {
-        auth: true,
-        anchorEl: null,
-    };
+  render() {
+    return (
+      <nav className="navbar navbar-light">
+        <div className="container">
 
-    render() {
+          <Link to="/" className="navbar-brand">
+            {this.props.appName.toLowerCase()}
+          </Link>
 
-        const { classes } = this.props;
+          <LoggedOutView currentUser={this.props.currentUser} />
 
-        /*return (
-         <nav className="navbar navbar-light">
-         <div className="container">
-
-         <Link to="/" className="navbar-brand">
-         {this.props.appName.toLowerCase()}
-         </Link>
-
-         <LoggedOutView currentUser={this.props.currentUser} />
-
-         <test currentUser={this.props.currentUser} />
-         </div>
-         </nav>
-         );*/
-
-        return (
-            <div className={classes.root}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="title" color="inherit" className={classes.flex}>
-                            <Link to="/" className="navbar-brand">
-                                {this.props.appName.toLowerCase()}
-                            </Link>
-                        </Typography>
-
-                        {!this.props.currentUser && (
-                            <div>
-                                <ul className="nav navbar-nav pull-xs-right">
-
-                                    <li className="nav-item">
-                                        <Link to="/" className="nav-link">
-                                            Home
-                                        </Link>
-                                    </li>
-
-                                    <li className="nav-item">
-                                        <Link to="/login" className="nav-link">
-                                            Sign in
-                                        </Link>
-                                    </li>
-
-                                    <li className="nav-item">
-                                        <Link to="/register" className="nav-link">
-                                            Sign up
-                                        </Link>
-                                    </li>
-
-                                </ul>
-                            </div>
-                        )}
-                        {this.props.currentUser && (
-                            <div>
-                                <ul className="nav navbar-nav pull-xs-right">
-
-                                    <li className="nav-item">
-                                        <Link to="/" className="nav-link">
-                                            Home
-                                        </Link>
-                                    </li>
-
-                                    <li className="nav-item">
-                                        <Link to="/editor" className="nav-link">
-                                            <i className="ion-compose"></i>&nbsp;New Post
-                                        </Link>
-                                    </li>
-
-                                    <li className="nav-item">
-                                        <Link to="/settings" className="nav-link">
-                                            <i className="ion-gear-a"></i>&nbsp;Settings
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link
-                                            to={`/@${this.props.currentUser.username}`}
-                                            className="nav-link">
-                                            <img src={this.props.currentUser.image} className="user-pic" alt={this.props.currentUser.username} />
-                                            {this.props.currentUser.username}
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        )}
-                    </Toolbar>
-                </AppBar>
-            </div>
-        );
-    }
+          <LoggedInView currentUser={this.props.currentUser} />
+        </div>
+      </nav>
+    );
+  }
 }
 
-Header.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Header);
+export default Header;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,200 +1,113 @@
+import { Link } from 'react-router-dom';
+import ListErrors from './ListErrors';
 import React from 'react';
 import agent from '../agent';
-import ListErrors from './ListErrors';
 import { connect } from 'react-redux';
 import {
-    UPDATE_FIELD_AUTH,
-    REGISTER,
-    REGISTER_PAGE_UNLOADED
+  UPDATE_FIELD_AUTH,
+  REGISTER,
+  REGISTER_PAGE_UNLOADED
 } from '../constants/actionTypes';
-import { withStyles } from 'material-ui/styles';
-import compose from 'recompose/compose';
-import Paper from 'material-ui/Paper';
-import TextField from 'material-ui/TextField';
-import Button from 'material-ui/Button';
 
 const mapStateToProps = state => ({ ...state.auth });
 
 const mapDispatchToProps = dispatch => ({
-    onChangeEmail: value =>
-        dispatch({ type: UPDATE_FIELD_AUTH, key: 'email', value }),
-    onChangePassword: value =>
-        dispatch({ type: UPDATE_FIELD_AUTH, key: 'password', value }),
-    onChangeUsername: value =>
-        dispatch({ type: UPDATE_FIELD_AUTH, key: 'username', value }),
-    onSubmit: (username, email, password) => {
-        const payload = agent.Auth.register(username, email, password);
-        console.log(payload);
-        dispatch({ type: REGISTER, payload })
-    },
-    onUnload: () =>
-        dispatch({ type: REGISTER_PAGE_UNLOADED })
-});
-
-const styles = theme => ({
-
-    root: theme.mixins.gutters({
-        paddingTop: 16,
-        paddingBottom: 16,
-        marginTop: theme.spacing.unit * 3,
-        marginLeft: theme.spacing.unit * 3,
-        marginRight: theme.spacing.unit * 3,
-    }),
-    titleBar: {
-        background:
-        'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
-        'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-    },
-    textField: {
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-        width: 200,
-    },
-    button: {
-        margin: theme.spacing.unit,
-    },
-    input: {
-        display: 'none',
-    },
+  onChangeEmail: value =>
+    dispatch({ type: UPDATE_FIELD_AUTH, key: 'email', value }),
+  onChangePassword: value =>
+    dispatch({ type: UPDATE_FIELD_AUTH, key: 'password', value }),
+  onChangeUsername: value =>
+    dispatch({ type: UPDATE_FIELD_AUTH, key: 'username', value }),
+  onSubmit: (username, email, password) => {
+    const payload = agent.Auth.register(username, email, password);
+    dispatch({ type: REGISTER, payload })
+  },
+  onUnload: () =>
+    dispatch({ type: REGISTER_PAGE_UNLOADED })
 });
 
 class Register extends React.Component {
-    constructor() {
-        super();
-        this.changeEmail = ev => this.props.onChangeEmail(ev.target.value);
-        this.changePassword = ev => this.props.onChangePassword(ev.target.value);
-        this.changeUsername = ev => this.props.onChangeUsername(ev.target.value);
-        this.submitForm = (username, email, password) => ev => {
-            ev.preventDefault();
-            this.props.onSubmit(username, email, password);
-        }
+  constructor() {
+    super();
+    this.changeEmail = ev => this.props.onChangeEmail(ev.target.value);
+    this.changePassword = ev => this.props.onChangePassword(ev.target.value);
+    this.changeUsername = ev => this.props.onChangeUsername(ev.target.value);
+    this.submitForm = (username, email, password) => ev => {
+      ev.preventDefault();
+      this.props.onSubmit(username, email, password);
     }
+  }
 
-    componentWillUnmount() {
-        this.props.onUnload();
-    }
+  componentWillUnmount() {
+    this.props.onUnload();
+  }
 
-    render() {
-        const email = this.props.email;
-        const password = this.props.password;
-        const username = this.props.username;
+  render() {
+    const email = this.props.email;
+    const password = this.props.password;
+    const username = this.props.username;
 
-      /*return (
-       <div className="auth-page">
-       <div className="container page">
-       <div className="row">
+    return (
+      <div className="auth-page">
+        <div className="container page">
+          <div className="row">
 
-       <div className="col-md-6 offset-md-3 col-xs-12">
-       <h1 className="text-xs-center">Sign Up</h1>
-       <p className="text-xs-center">
-       <Link to="/login">
-       Have an account?
-       </Link>
-       </p>
+            <div className="col-md-6 offset-md-3 col-xs-12">
+              <h1 className="text-xs-center">Sign Up</h1>
+              <p className="text-xs-center">
+                <Link to="/login">
+                  Have an account?
+                </Link>
+              </p>
 
-       <ListErrors errors={this.props.errors} />
+              <ListErrors errors={this.props.errors} />
 
-       <form onSubmit={this.submitForm(username, email, password)}>
-       <fieldset>
+              <form onSubmit={this.submitForm(username, email, password)}>
+                <fieldset>
 
-       <fieldset className="form-group">
-       <input
-       className="form-control form-control-lg"
-       type="text"
-       placeholder="Username"
-       value={this.props.username}
-       onChange={this.changeUsername} />
-       </fieldset>
-
-       <fieldset className="form-group">
-       <input
-       className="form-control form-control-lg"
-       type="email"
-       placeholder="Email"
-       value={this.props.email}
-       onChange={this.changeEmail} />
-       </fieldset>
-
-       <fieldset className="form-group">
-       <input
-       className="form-control form-control-lg"
-       type="password"
-       placeholder="Password"
-       value={this.props.password}
-       onChange={this.changePassword} />
-       </fieldset>
-
-       <button
-       className="btn btn-lg btn-primary pull-xs-right"
-       type="submit"
-       disabled={this.props.inProgress}>
-       Sign up
-       </button>
-
-       </fieldset>
-       </form>
-       </div>
-
-       </div>
-       </div>
-       </div>
-       );*/
-
-        const { classes } = this.props;
-
-        return (
-            <div>
-
-              <Paper className={classes.root} elevation={4}>
-
-                <ListErrors errors={this.props.errors} />
-
-                <form noValidate onSubmit={this.submitForm(username, email, password)} autoComplete="off" autoFill="off">
-                  <TextField
-                      required
-                      id="name"
-                      label="Name"
+                  <fieldset className="form-group">
+                    <input
+                      className="form-control form-control-lg"
                       type="text"
                       placeholder="Username"
                       value={this.props.username}
-                      onChange={this.changeUsername}
-                      className={classes.textField}
-                      margin="normal"
-                  />
-                  <TextField
-                      required
-                      id="email"
-                      label="Email"
+                      onChange={this.changeUsername} />
+                  </fieldset>
+
+                  <fieldset className="form-group">
+                    <input
+                      className="form-control form-control-lg"
                       type="email"
                       placeholder="Email"
                       value={this.props.email}
-                      onChange={this.changeEmail}
-                      className={classes.textField}
-                      margin="normal"
-                  />
-                  <TextField
-                      required
-                      id="required"
-                      label="Required"
+                      onChange={this.changeEmail} />
+                  </fieldset>
+
+                  <fieldset className="form-group">
+                    <input
+                      className="form-control form-control-lg"
                       type="password"
                       placeholder="Password"
                       value={this.props.password}
-                      onChange={this.changePassword}
-                      className={classes.textField}
-                      margin="normal"
-                  />
+                      onChange={this.changePassword} />
+                  </fieldset>
 
-                  <Button variant="raised" color="primary" type="submit" className={classes.button}  disabled={this.props.inProgress}>
+                  <button
+                    className="btn btn-lg btn-primary pull-xs-right"
+                    type="submit"
+                    disabled={this.props.inProgress}>
                     Sign up
-                  </Button>
-                </form>
-              </Paper>
+                  </button>
+
+                </fieldset>
+              </form>
             </div>
-        );
-    }
+
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default compose(
-    withStyles(styles, { withTheme: true }, { name: 'Register' }),
-    connect(mapStateToProps, mapDispatchToProps)
-)(Register);
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
